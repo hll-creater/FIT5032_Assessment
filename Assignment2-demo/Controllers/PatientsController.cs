331758e654d14786ff149a77fcabbcf2293ba377
tuixiaofei <tuixiaofei@icloud.com>
2023-07-30 13:29:59 +0800

    demo

---
+++ Assignment2-demo/Controllers/PatientsController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Assignment2.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace Assignment2.Controllers
+{
+    [Authorize(Roles = "Patient,Admin")]
+    public class PatientsController : Controller
+    {
+        private AppointmentContainer db = new AppointmentContainer();
+        private ApplicationDbContext identityDB = new ApplicationDbContext();
+
+        // GET: Patients
+        [Authorize]
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var patients = db.Patients.Where(s => s.UserId ==
+            userId).ToList();
+
+            //admin can view every list
+            if (User.IsInRole("Admin"))
+            {
+                patients = db.Patients.ToList();
+                //userId = User.Select(m => m.userId).ToList();
+            }
+
+            var loggedInUser= identityDB.Users.FirstOrDefault(u => u.Id  == userId);
+            ApplicationUserManager _userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var temp = _userManager.FindById("4eebc740-bfdb-4a6e-bbe9-69af6f8e64f8").UserName;
+
+            var patientViewList = patients.Select(p => new PatientViewModel
+            {
+                Id = p.Id,
+                FirstName = p.FirstName,
+                LastName = p.LastName,
+                Email = _userManager.FindById(p.UserId).UserName,
+            }).ToList();
+
+       
+            
+            return View(patientViewList);
+        }
+
+        // GET: Patients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // GET: Patients/Create
+        [AllowAnonymous]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName")] Patient patient)
+        {
+            patient.UserId = User.Identity.GetUserId();
+            ModelState.Clear();
+            TryValidateModel(patient);
+            if (ModelState.IsValid)
+            {
+                db.Patients.Add(patient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(patient);
+        }
+
+        // GET: Patients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        // POST: Patients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,UserId")] Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(patient);
+        }
+
+        [Authorize(Roles = "Admin")]
+        // GET: Patients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patient patient = db.Patients.Find(id);
+            if (patient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patient);
+        }
+
+        [Authorize(Roles = "Admin")]
+        // POST: Patients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Patient patient = db.Patients.Find(id);
+            db.Patients.Remove(patient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Assignment2-demo/Models/SendEmailViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Models
+{
+    public class SendEmailViewModel
+    {
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Please enter an email address.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string ToEmail { get; set; }
+
+        [Required(ErrorMessage = "Please enter a subject.")]
+        public string Subject { get; set; }
+
+        [AllowHtml]
+        [Required(ErrorMessage = "Please enter the contents")]
+        public string Contents { get; set; }
+
+        public HttpPostedFileBase Attachment { get; set; }
+
+    }
+}
